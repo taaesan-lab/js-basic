9b1eee4b8665b695b7d45df6226c952cfd4ff630
taaesan <taaesan@gmail.com>
2021-09-02 21:01:20 +0700

    time inputting

--- 30days/day4-class-instance.js
+++ 30days/day4-class-instance.js
@@ -1,22 +1,42 @@
-function Person(initialAge){
-    let age = initialAge;
-      // Add some more code to run some checks on initialAge
-    this.amIOld=function(){
-     // Do some computations in here and print out the correct statement to the console
-     let text = 'You are old.'
-      if (this.age < 0){
-          text = 'Age is not valid, setting age to 0.';
-      }else if(this.age < 13){
-          text = 'You are young.';
-      }else if (this.age >= 13 && this.age < 18){
-          text = 'You are a teenager.';
-      }
-      console.log(text);
-    };
-     this.yearPasses=function(){
-            // Increment the age of the person in here
-          this.age++;
-     };
-  }
+class Person {
 
-new Person(4)
+  constructor(initialAge){
+    this.age = initialAge;
+  }
+  // Add some more code to run some checks on initialAge
+  amIOld() {
+    // Do some computations in here and print out the correct statement to the console
+    if (this.age < 0) {
+      console.log('Age is not valid, setting age to 0.');
+      console.log('You are young.');
+    } else if (this.age < 13) {
+      console.log('You are young.');
+    } else if (this.age >= 13 && this.age < 18) {
+      console.log('You are a teenager.');
+    } else {
+      console.log('You are old.');
+    }
+  };
+
+  yearPasses() {
+    // Increment the age of the person in here
+    this.age++;
+  };
+}
+
+function main() {
+  var T = parseInt(2);
+  let items = [-5, 30]
+  for (i = 0; i < T; i++) {
+    var age = items[i];
+    var p = new Person(age);
+    p.amIOld();
+    for (j = 0; j < 3; j++) {
+      p.yearPasses();
+
+    }
+    p.amIOld();
+    console.log("");
+  }
+}
+main();
---
+++ _timeinputting/index.js
@@ -0,0 +1,5 @@
+document.getElementById('Document_FromDate').value = '31 Aug 2021';
+document.getElementById('Document_TimeInputting_TimeIn').value = '08:30';
+document.getElementById('Document_TimeInputting_TimeOut').value = '17:00';
+document.getElementById('Document_TimeInputting_Reason').value = 'Work form home';
+document.getElementById('Document_TimeInputting_Place').value = 'Home';
