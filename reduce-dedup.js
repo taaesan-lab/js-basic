4e90bca1afaf4bd4f42ddcaa73efd69cbe686b20
taaesan <taaesan@gmail.com>
2022-03-12 11:28:26 +0700

    reduce-dedup

--- reduce-dedup.js
+++ reduce-dedup.js
@@ -1,14 +1,14 @@
 const dedupNumbers = (nums) => {
-    // return nums.reduce((p,c,i) => p.includes(c) ? p : p.concat(c),[]);
-    return nums.reduce((p,c,i) => {
-        if (p.includes(c)) {
-            return p;
-        } else {
-            return p.concat(c);
-        }
-    },[]);
-}
+  // return nums.reduce((p,c,i) => p.includes(c) ? p : p.concat(c),[]);
+  return nums.reduce((p, c, i) => {
+    if (p.includes(c)) {
+      return p;
+    } else {
+      return p.concat(c);
+    }
+  }, []);
+};
 
-// console.log(dedupNumbers([1,1,2,3]))
+console.log(dedupNumbers([1, 1, 2, 3]));
 
 module.exports = dedupNumbers;
